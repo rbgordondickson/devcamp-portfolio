347f5d3b2ce4099e974c2c632f9f019836c491b0
rbgordondickson <rb.gordondickson@gmail.com>
2020-04-13 15:51:41 +0100

    Worked with debugging tools

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,12 +1,10 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item , only: [:edit, :update, :show, :destroy]
   layout "portfolio"
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
   def destroy
-    #Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
     #Perform detroy/delete
     @portfolio_item.destroy
     respond_to do |format|
@@ -21,11 +19,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
@@ -59,3 +55,7 @@ class PortfoliosController < ApplicationController
                                       )
   end
 end
+
+def set_portfolio_item
+  @portfolio_item = Portfolio.find(params[:id])
+end 
